fb4a6ccb09a9efeb61c3a2c561bccc7cfda92127
Karolina K <63045938+FlamingLlama@users.noreply.github.com>
2020-12-11 11:52:01 +0000

    Update lrCostFunction.m

--- assignment_3/lrCostFunction.m
+++ assignment_3/lrCostFunction.m
@@ -36,14 +36,14 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+h = sigmoid(X * theta);
 
+J = (1 / m) * (-y' * log(h) - (1 - y)' * log(1 - h)) + (lambda/(2 * m)) * (theta(2:length(theta)))' * theta(2:length(theta));
 
+theta_0 = theta;
+theta_0(1) = 0;
 
-
-
-
-
-
+grad = ((1 / m) * (h - y)' * X) + (lambda / m * theta_0');
 
 % =============================================================
 
